50733fd6d5e15c16779a6a05ba75cd2d23e48df9
SaharshSamir <saharshsamir10@gmail.com>
2021-02-28 10:08:12 +0400

    instantiated server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})

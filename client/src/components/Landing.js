3f5b31e13b28b7b4c2948797db85aaca3bbeb6f8
SaharshSamir <saharshsamir10@gmail.com>
2021-03-07 03:27:46 +0400

    client socket not connecting

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-class Landing extends Component {
-    render() {
-        return (
-            <div className="container">
-                <button className="btn">Join Room</button>
-                <button className="btn">Create Room</button>
-            </div>
-        )
+let socket;
+const Landing = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    socket = io.connect('http://localhost:3000');
+    console.log(socket);
+
+    const setUsername = e => {
+        setName(e.target.value);
     }
+    const setRoomName = e => {
+        setRoom(e.target.value);
+    }
+
+    const submitFunc = (e) => {
+        e.preventDefault();
+        console.log('submitted');
+        socket.emit('addUser', { name, room });
+    }
+    return (
+        <div className="container">
+            <form onSubmit={e => submitFunc(e)}>
+                <input placeholder="Username" id="nameInput" onChange={e => setUsername(e)} />
+                <input placeholder="Room Name" id="roomInput" onChange={e => setRoomName(e)} />
+                <button className="btn btn-primary" type="submit">Join</button>
+            </form>
+        </div>
+    );
 }
 
 export default Landing;
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
+const socket = require("socket.io");
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+const io = socket(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', socket => {
+    console.log('user has connected');
+})
+
+app.get("/", (req, res) => {
+    res.send({ hi: "there" });
 })
 
 app.get("/room/:id", (req, res) => {
     res.send({
         "roomId": req.params.id
     })
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 })

a8faf69b446cd2786ae9640382c1e2df2b3a42da
SaharshSamir <saharshsamir10@gmail.com>
2021-04-17 05:59:28 +0400

    prod 2.1

--- index.js
+++ index.js
@@ -9,7 +9,33 @@ app.get("/", (req, res) => {
     domainName = fullUrl;
     console.log(fullUrl + 'line8');
 })
-require('./routes/routes')(app);
+
+app.get("/room/:id", (req, res) => {
+    res.send({
+        "roomId": req.params.id
+    })
+})
+
+
+app.get('/api/endpoint', (req, res) => {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
+    {
+        res.send({ ENDPOINT: "http://localhost:5000" })
+    } else
+    {
+        res.send({ ENDPOINT: "" })
+    }
+})
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+// require('./routes/routes')(app);
 
 
 
